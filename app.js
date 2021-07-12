6df6c855287a6ec78aa566bd367f398e83b2de90
matheustakasaki <matheus8antunes@gmail.com>
2021-07-11 23:48:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const books = require('./database')
+
+console.log(books);
---
+++ database.js
@@ -0,0 +1,30 @@
+const books = [
+    {
+        id: 1,
+        name: 'Aleatorio',
+        author: 'José do Egito',
+        category: 'Filosofia',
+        pages: 200,
+        recommended: true,
+        read: true,
+    },
+    {
+        id: 2,
+        name: 'Aleatorio II',
+        author: 'José do Egito',
+        category: 'Filosofia',
+        pages: 150,
+        recommended: true,
+        read: true,
+    },
+    {
+        id: 3,
+        name: 'Aleatorio III',
+        author: 'José do Egito',
+        category: 'Filosofia',
+        pages: 256,
+        recommended: false,
+        read: true,
+    }
+]
+module.exports = books
